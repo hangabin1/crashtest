ead07e4c53da0ff118db3e90082321bbe4d32ec7
hangabin1 <hgb101931@g.hongik.ac.kr>
2026-01-08 11:46:23 +0900

    first commit

---
+++ src/main/java/com/meta/crashpractice/CrashpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.meta.crashpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrashpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrashpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meta/crashpractice/CrashpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.crashpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrashpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
